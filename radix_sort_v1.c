2d0f66fe1ed86907618bed1206ee919acadac9c9
joaopelison <42846276+joaopelison@users.noreply.github.com>
2018-11-27 22:07:26 -0300

    radix_sort_v3.c

--- radix_sort_v1.c
+++ radix_sort_v1.c
@@ -2,47 +2,123 @@
 #include <stdlib.h>
 #include <string.h>
 
+//criação de uma estrutura de uma lista, que irá receber as palavras em forma de lista duplamente encadeada, onde cada nó será uma letra.
+typedef struct Letra{
+    char letra;
+    int tamanho;
+    struct Letra *prox;
+ 	struct Letra *ant;
+}Letra;
+ 
+ //criação de um TAD, que irá organizar as letras da minha palavra em forma de lista.
+typedef struct Palavra{
+    struct Letra *Inicio;
+    struct Letra *Fim;
+    struct Palavra *prox;
+}Palavra;
 
-typedef struct SNo{
-	char palavra[];
-	int tamanho;
-	struct SNo *prox;
-}SNo;
-
-typedef struct Lista{
-	struct SNo *Inicio;
-	struct SNo *Fim;	
-}Lista;
-
-Lista criaLista(){
-	Lista *L = (Lista *)malloc(sizeof(Lista));
-	L->Inicio = L->Fim = NULL;
-	return L;
+//criação de um TAD, que irá organizar as palavras em uma frase por forma de lista.
+typedef struct Frase{
+	struct Palavra *Inicio;
+	struct Palavra *Fim;
+}Frase;
+ 
+ //Função que aloca espaço na memória para cada uma das minhas palavras.
+Palavra * criaPalavra(){
+    
+    Palavra *P =(Palavra *)malloc(sizeof(Palavra));
+    P->Inicio = P->Fim = NULL;
+  
+    return P;
 }
 
-void inserePalavra(char palavra, Lista *L){
-	SNo *Novo = (SNo *)malloc(sizeof(SNo));
-	Novo->palavra = palav;
-	Novo->prox = NULL;
-	Novo->tamanho = strlen(palavra);
-	if(L->Inicio == NULL){
-		L->Inicio = Novo;
+//Inserção das letras na Palavra.
+void inserePalavra(char l, Palavra *P){
+ 
+   		Letra *Novo = (Letra *)malloc(sizeof(Letra));
+    	Novo->letra= l;
+    	Novo->prox = Novo->ant = NULL;
+ 	 //  Novo->tamanho = strlen (palavra);
+  
+    	if (P->Inicio == NULL){
+    	    P->Inicio = Novo;
+    	}
+    	else {
+    	    P->Fim->prox = Novo;
+    	    Novo->ant = P->Fim;
+    	}
+    	P->Fim = Novo;	
+  }
+
+void imprimeP(Palavra *P){
+	Letra *temp  = P->Inicio;
+	
+	while(temp != NULL){
+		printf("%c ->",temp->letra);
+		temp = temp->prox;
+	}
+}
+
+//Alocando espaço na memória para a frase.
+Frase * criaFrase(){
+    
+    Frase *F = (Frase *)malloc(sizeof(Frase));
+    F->Inicio = F->Fim = NULL;
+  
+    return F;
+}
+
+//Inserção das palavras na Frase.
+void entradaFrase (Palavra *P, Frase *F){
+	
+	if(F->Inicio == NULL){
+		F->Inicio =  P;
 	}else{
-		L->Fim->prox = Novo;
+		F->Fim->prox = P;
 	}
-	L->Fim = Novo;
+	F->Fim = P;
 }
 
-int main(){
+
+
+/*void inserePalavra (char palavra, Fila *F){
+	int qtdPalav, i;
+	char palavra;
+	printf("Informe a quantidade de palavras que serão inseridas: ");
+    scanf("%i",&qtdPalav);
+    printf("\n");
+    for(i = 0; i < qtdPalav; i++){
+    	fgets(palavra, 50, stdin);
+    	enfileiraPalavra(palavra, F);
+	}
+}
+
+int i;
+Fila *vet_baldes[10];
+    for(i=0;i<10;i++){
+    vet_baldes[i] = criaFila();
+}
+
+ /*radixSort(){
 	
-	char palav[];
+    	
+    	
+
+}
+*/
+
+int main (){
+	Palavra *P = criaPalavra();
+	int i=0;
+	char ent[50];
+	fgets(ent,50,stdin);
 	
-	while(palavra[] != '.'){
-		
-		
-		
+	while(ent[i] != '\n'){
+		inserePalavra(ent[i],P);
+		i++;
 	}
 	
-	
-		
+   	imprimeP(P);
+ 
+ 
 }
